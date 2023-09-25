63d8a65b961a9d97f1a7fa3ad0297e16c63dc9fd
Jaclyn Beck <jaclyn.beck@sagebase.org>
2023-09-25 19:58:48 +0000

    Added map reference to upload scripts

--- upload_scripts/UploadInputToSynapse.R
+++ upload_scripts/UploadInputToSynapse.R
@@ -7,6 +7,9 @@ synLogin()
 input_folder <- Folder("input", parent = "syn49332774")
 input_folder <- synStore(input_folder, forceVersion = FALSE)
 
+map_folder <- Folder("map_reference", parent = "syn49332774")
+map_folder <- synStore(map_folder, forceVersion = FALSE)
+
 provenance <- list("cain" = c("syn23554294", "syn23554292", "syn23554293"),
                    "lau" = c("syn38609700", "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE157827&format=file"),
                    "leng" = c("syn22722817", "syn22722860"),
@@ -26,13 +29,25 @@ provenance <- list("cain" = c("syn23554294", "syn23554292", "syn23554293"),
 datasets <- c("cain", "lau", "leng", "mathys", #"morabito",
               "seaRef", "seaAD", "Mayo", "MSBB", "ROSMAP")
 
+# Helper function
+uploadFile <- function(filename, parent_folder, dataset) {
+  if (!(file.info(filename)$isdir)) { # Exclude directories
+    file <- File(path = filename, parent = parent_folder)
+    file <- synStore(file, used = provenance[[dataset]], forceVersion = FALSE)
+    print(paste0(file$properties$id, ": ", file$properties$name))
+  }
+}
+
+# Processed single cell and bulk datasets
 for (dataset in datasets) {
   files <- list.files(dir_input, pattern = dataset, full.names = TRUE)
   for (filename in files) {
-    if (!(file.info(filename)$isdir)) { # Exclude directories
-      file <- File(path = filename, parent = input_folder)
-      file <- synStore(file, used = provenance[[dataset]], forceVersion = FALSE)
-      print(paste0(file$properties$id, ": ", file$properties$name))
-    }
+    uploadFile(filename, input_folder, dataset)
   }
+}
+
+# Cell type mapping reference
+files <- list.files(dir_map_reference, full.names = TRUE)
+for (filename in files) {
+  uploadFile(filename, map_folder, "cain")
 }
