5d4ec429658501488ceda62f1acccf92b06676b9
Jaclyn Beck <jaclyn.beck@sagebase.org>
2023-10-11 21:40:12 +0000

    Updated error upload script

--- upload_scripts/UploadErrorsToSynapse.R
+++ upload_scripts/UploadErrorsToSynapse.R
@@ -6,37 +6,63 @@ source("Filenames.R")
 synLogin()
 
 # Deconvolution WG Synapse space
-errors.folder <- Folder("error_calculations", parent = "syn49332774")
-errors.folder <- synStore(errors.folder, forceVersion = FALSE)
+errors_folder <- Folder("error_calculations", parent = "syn49332774")
+errors_folder <- synStore(errors_folder, forceVersion = FALSE)
 
-# Get provenance IDs
+# Get provenance IDs from algorithm output folder
 bulk_folders <- as.list(synGetChildren("syn52245555"))
-params_lists <- sapply(bulk_folders, function(X) {
-  as.list(synGetChildren(X$id))
+
+# Folder structure on Synapse is <bulk_dataset>/<algorithm>/<params_file>
+params_list_df <- lapply(bulk_folders, function(B) {
+  algorithms <- as.list(synGetChildren(B$id))
+
+  # Get the files under each algorithm folder
+  params_files <- lapply(algorithms, function(A) {
+    params_lists <- as.list(synGetChildren(A$id))
+    if (length(params_lists) == 0) {
+      return(NULL)
+    }
+    df <- data.frame(do.call(rbind, params_lists)) %>% select(name, id)
+    df$test_dataset <- B$name
+    return(df)
+  })
+
+  params_files <- do.call(rbind, params_files)
 })
 
-params_list_df <- data.frame(do.call(rbind, params_lists)) %>%
-  select(name, id)
+params_list_df <- do.call(rbind, params_list_df)
 
-fields <- str_split(params_list_df$name, "_", simplify = TRUE)
-params_list_df$reference_dataset <- fields[,3]
-params_list_df$algorithm <- fields[,2]
-params_list_df$test_dataset <- fields[,4]
-params_list_df$granularity <- fields[,5]
+params_list_df$name_base <- str_replace(unlist(params_list_df$name), "estimates_", "")
+params_list_df$name_base <- str_replace(params_list_df$name_base, ".rds", "")
 
-files <- list.files(dir_errors)
+# Loop over each data set and algorithm
+for (test_dataset in unique(params_list_df$test_dataset)) {
+  bulk_errors_folder <- Folder(test_dataset, parent = errors_folder)
+  bulk_errors_folder <- synStore(bulk_errors_folder)
 
-# Parameter output lists
-for (filename in files) {
-  fields <- unlist(str_split(filename, "_"))
-  provenance <- subset(params_list_df, reference_dataset == fields[3] &
-                         test_dataset == fields[4] &
-                         granularity == str_replace(fields[5], "\\.rds", "") &
-                         algorithm == fields[2])
+  algorithms <- list.files(file.path(dir_errors, test_dataset))
 
-  file <- File(path = file.path(dir_errors, filename), parent = errors.folder)
-  file <- synStore(file,
-                   used = unlist(provenance$id),
-                   forceVersion = FALSE)
-  print(paste0(file$properties$id, ": ", file$properties$name))
+  for (algorithm in algorithms) {
+    algorithm_errors_folder <- Folder(algorithm, parent = bulk_errors_folder)
+    algorithm_errors_folder <- synStore(algorithm_errors_folder)
+
+    dir_alg <- file.path(dir_errors, test_dataset, algorithm)
+
+    files <- list.files(dir_alg)
+
+    # Parameter output lists
+    for (filename in files) {
+      # The name of the error file should match the name of the algorithm output file
+      error_name_base <- str_replace(filename, "errors_", "")
+      error_name_base <- str_replace(error_name_base, ".rds", "")
+
+      provenance <- subset(params_list_df, name_base == error_name_base)
+
+      file <- File(path = file.path(dir_alg, filename), parent = algorithm_errors_folder)
+      file <- synStore(file,
+                       used = unlist(provenance$id),
+                       forceVersion = FALSE)
+      print(paste0(file$properties$id, ": ", file$properties$name))
+    }
+  }
 }
