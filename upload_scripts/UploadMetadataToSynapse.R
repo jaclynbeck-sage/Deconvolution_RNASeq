5b4d6dd087d01dba57eab9218b2a8f420846d4c1
Jaclyn Beck <jaclyn.beck@sagebase.org>
2023-12-14 23:05:35 +0000

    Fixed metadata upload script with seaRef gtf link

--- upload_scripts/UploadMetadataToSynapse.R
+++ upload_scripts/UploadMetadataToSynapse.R
@@ -9,8 +9,8 @@ meta_folder <- synStore(meta_folder, forceVersion = FALSE)
 
 provenance <- list("genes" = c("syn26967452", # ROSMAP, Mayo, MSBB gene conversions
                                "syn18687959", # Mathys gene conversions
+                               "https://brainmapportal-live-4cc80a57cd6e400d854-f7fdcae.divio-media.net/filer_public/67/39/67390730-a684-47a5-b9f4-89c47cd4e3fc/genesgtf.gz", # seaRef gene conversions
                                "https://ftp.ensembl.org/pub/release-98/gtf/homo_sapiens/Homo_sapiens.GRCh38.98.gtf.gz",
-                               "https://ftp.ensembl.org/pub/release-94/gtf/homo_sapiens/Homo_sapiens.GRCh38.94.gtf.gz",
                                "https://ftp.ensembl.org/pub/release-93/gtf/homo_sapiens/Homo_sapiens.GRCh38.93.gtf.gz",
                                "https://ftp.ensembl.org/pub/release-84/gtf/homo_sapiens/Homo_sapiens.GRCh38.84.gtf.gz"),
                    "ihc" = "https://github.com/ellispatrick/CortexCellDeconv.git")
