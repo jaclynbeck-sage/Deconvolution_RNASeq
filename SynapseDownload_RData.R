99c66e6dcfc561e2d57fed181df7e5b1842240fd
Jaclyn Beck <jaclyn.beck@sagebase.org>
2024-07-11 23:20:38 +0000

    Fixed typo in download script

--- SynapseDownload_RData.R
+++ SynapseDownload_RData.R
@@ -17,9 +17,9 @@ folders <- list("metadata" = list("synID" = "syn58803307", "downloadLocation" = 
                 "Mayo" = list("synID" = "syn59490216", "downloadLocation" = file.path(dir_estimates, "Mayo")),
                 "MSBB" = list("synID" = "syn59490283", "downloadLocation" = file.path(dir_estimates, "MSBB")),
                 "ROSMAP" = list("synID" = "syn59490358", "downloadLocation" = file.path(dir_estimates, "ROSMAP")),
-                "errors" = list("synID" = "syn60969684", "downloadLocation" = dir_errors),
+                "errors" = list("synID" = "syn60969684", "downloadLocation" = dir_errors)
                 # uncomment only if these files are needed
-                #"hspe_params" = list("synID" = "syn61709305, "downloadLocation" = dir_hspe_params),
+                #"hspe_params" = list("synID" = "syn61709305", "downloadLocation" = dir_hspe_params),
                 #"music_basis" = list("synID" = "syn59500238", "downloadLocation" = dir_music_basis),
                 #"scaden_models" = list("synID" = "syn61551467", "downloadLocation" = dir_scaden_models)
                 )
