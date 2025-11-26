6627c2809bf08ea31bbbb37e9d15596dfd9fd1e7
Jaclyn Beck <jaclyn.beck@sagebase.org>
2025-11-26 03:52:25 +0000

    Added DWLS config, removed old R config files

--- Step00_InitialSetupInstall.R
+++ Step00_InitialSetupInstall.R
@@ -12,8 +12,9 @@ install.packages(
 )
 
 # Extra setup to get synapser to install -- Downgrade to Python 3.11 and install
-# rjson version 0.2.21
-reticulate::install_python(version = "3.11")
+# rjson version 0.2.21. Freezing at specific Python version 3.11.12 which is
+# the version used to run data for the paper.
+reticulate::install_python(version = "3.11.12")
 reticulate::virtualenv_create("r-reticulate")
 
 remotes::install_version("rjson", version = "0.2.21")
@@ -68,11 +69,19 @@ remotes::install_github("jaclynbeck-sage/omnideconv", ref = "use_virtualenv",
                         clean = TRUE)
 remotes::install_github("omnideconv/DWLS",
                         clean = TRUE)
-omnideconv::install_all_python()
 
-# Further r-omnideconv virtualenv setup to ensure package versions are compatible.
-# requirements file was generated with `pip freeze > omnideconv_requirements.txt`
-# in an existing r-omnideconv environment that had already been set up and
-# confirmed to work.
-reticulate::virtualenv_install("r-omnideconv",
-                               requirements = file.path("docker", "omnideconv_requirements.txt"))
+# Ensure that pip manages all dependencies at once for all packages that need to
+# get installed for omnideconv. Calling omnideconv::install_all_python() does
+# the installation piecemeal and results in an incompatible environment.
+#reticulate::virtualenv_create("r-omnideconv", version = "3.11.12", force = TRUE,
+#                              packages = c("numpy", "anndata", "scanpy",
+#                                           "scikit-learn", "scikit-misc",
+#                                           "git+https://github.com/omnideconv/AutoGeneS.git",
+#                                           "git+https://github.com/omnideconv/scaden.git"))
+
+# After running the above virtualenv_create and confirming that the resulting
+# environment works, I used `pip freeze > omnideconv_requirements.txt` to get a
+# requirements file. This will ensure that the exact package versions used are
+# always installed in the docker container.
+reticulate::virtualenv_create("r-omnideconv",
+                              requirements = file.path("docker", "omnideconv_requirements.txt"))
