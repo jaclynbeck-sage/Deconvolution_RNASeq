046b9b24947ffc4ac694aba472d23f3908d165ab
Jaclyn Beck <jaclyn.beck@sagebase.org>
2026-05-13 22:44:10 +0000

    Updated Step11 upload script

--- upload_scripts/Step11_UploadTopParameters.R
+++ upload_scripts/Step11_UploadTopParameters.R
@@ -4,39 +4,33 @@ source("Filenames.R")
 source(file.path("upload_scripts", "Upload_HelperFunctions.R"))
 
 # Deconvolution WG Synapse space
-top_params_folder <- Folder("11_top_parameters", parent = "syn68238853")
-top_params_folder <- synStore(top_params_folder, forceVersion = FALSE)
+top_params_folder <- Folder(basename(dir_top_parameters),
+                            parent = config::get("upload_synid")) |>
+  synStore()
+
+top_errors_folder <- Folder(basename(dir_top_errors),
+                            parent = config::get("upload_synid")) |>
+  synStore()
+
+top_estimates_folder <- Folder(basename(dir_top_estimates),
+                               parent = config::get("upload_synid")) |>
+  synStore()
 
 # Provenance TODO
 
-github <- "https://github.com/jaclynbeck-sage/Deconvolution_RNASeq/blob/main/Step11_Get_TopParamSets.R"
+github <- paste0(config::get("github_repo_url"), "Step11_Get_TopParamSets.R")
 
 # Loop over each data set and algorithm
-for (test_dataset in c("Mayo", "MSBB", "ROSMAP")) {
-  bulk_params_folder <- Folder(test_dataset, parent = top_params_folder)
-  bulk_params_folder <- synStore(bulk_params_folder)
+for (test_dataset in all_bulk_datasets()) {
+  RecursiveUpload(folder_name = file.path(dir_top_parameters, test_dataset),
+                  parent = top_params_folder,
+                  provenance = list("executed" = github))
 
-  algorithms <- list.files(file.path(dir_top_parameters, test_dataset))
+  RecursiveUpload(folder_name = file.path(dir_top_errors, test_dataset),
+                  parent = top_errors_folder,
+                  provenance = list("executed" = github))
 
-  for (algorithm in algorithms) {
-    algorithm_params_folder <- Folder(algorithm, parent = bulk_params_folder)
-    algorithm_params_folder <- synStore(algorithm_params_folder)
-
-    dir_alg <- file.path(dir_top_parameters, test_dataset, algorithm)
-
-    files <- list.files(dir_alg, full.names = TRUE)
-
-    # Parameter output lists
-    for (filename in files) {
-      # The name of the param file should match the name of the algorithm output file
-      #param_name_base <- str_replace(filename, "errors_", "")
-      #param_name_base <- str_replace(param_name_base, ".rds", "")
-
-      #provenance <- subset(params_list_df, name_base == error_name_base)
-
-      UploadFile(filename,
-                 parent_folder = algorithm_params_folder,
-                 provenance = list("used" = c(), "executed" = github))
-    }
-  }
+  RecursiveUpload(folder_name = file.path(dir_top_estimates, test_dataset),
+                  parent = top_estimates_folder,
+                  provenance = list("executed" = github))
 }
